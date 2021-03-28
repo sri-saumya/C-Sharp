553f017505c3dd1ab2272680ddd8070e57310bf1
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-28 12:52:12 -0700

    Override ToString() method

---
+++ Lab-8/Override ToString()/Override ToString()/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Override_ToString__
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product()
+            {
+                Id = 101,
+                Name = "Pen",
+                Price = 5000
+            };
+
+            Console.WriteLine(p.ToString());
+
+        }
+    }
+}
---
+++ Lab-8/Override ToString()/Override ToString()/product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Override_ToString__
+{
+
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            string s = this.Id + " " + this.Name + " " + this.Price;
+
+            return s;
+        }
+
+    }
+}
