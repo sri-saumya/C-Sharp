458dd99ee9788c8982906271615da988fc350e39
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-28 11:35:13 -0700

    Banking Example

---
+++ Lab-2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using ClassLibrary1;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc1 = new BankAccount("Foo", 100000);
+            //Console.WriteLine(acc1.Balance);
+            acc1.MakeWithDrawal(5000, DateTime.Now, "first w");
+            //Console.WriteLine(acc1.Balance);
+            try
+            {
+                acc1.MakeWithDrawal(6000, DateTime.Now, "first deposite");
+                //Console.WriteLine(acc1.Balance);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine(acc1.GetAccountHistory());
+            Console.WriteLine("FOR ACCOUNT 2");
+
+            var acc2 = new BankAccount("FFF", 500000);
+            //Console.WriteLine(acc1.Balance);
+            acc1.MakeWithDrawal(5000, DateTime.Now, "SECOND w");
+            //Console.WriteLine(acc1.Balance);
+            try
+            {
+                acc1.MakeWithDrawal(6000, DateTime.Now, "SECOND deposite");
+                //Console.WriteLine(acc1.Balance);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine(acc2.GetAccountHistory());
+
+        }
+    }
+}
