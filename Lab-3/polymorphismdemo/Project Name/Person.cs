60c6ca462984f9a8dcc7417b872b699113f8a584
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-28 12:08:17 -0700

    Polymorphism

---
+++ Lab-3/polymorphismdemo/Project Name/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Name
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+    
+
+        //private string name;
+        //public string _Name
+        //{
+        //    get => name;
+        //    set => name = value;
+        //}
+
+        public override string ToString()
+        {
+            string s = Name + " is " + Age + " year old " ;
+            Console.WriteLine(s);
+            return s;
+        }
+    }
+}
+
---
+++ Lab-3/polymorphismdemo/Project Name/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Person p = new Person("Saumya", 15 );
+            p.ToString();
+        }
+    }
+}
