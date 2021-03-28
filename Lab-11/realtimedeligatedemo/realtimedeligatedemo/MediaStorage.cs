07495a87a5f3574be94f41e172edff72b34574a4
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-28 13:10:39 -0700

    Media Player to demonstrate delegate

---
+++ Lab-11/realtimedeligatedemo/realtimedeligatedemo/MediaStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace realtimedelegatedemo
+{
+    public class MediaStorage
+    {
+        public delegate int PlayerMedia(string a);
+
+        public void ReportResult(PlayerMedia p , string type)
+        {
+            var status = p(type);
+            if(status == 0)
+            {
+                Console.WriteLine("It will play");
+            }
+            else
+            {
+                Console.WriteLine("It will not play");
+            }
+        }
+
+    }
+}
