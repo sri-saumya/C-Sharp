efd7f179939b76c4dcedef7bf0085d08e37cc6f3
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-28 12:27:31 -0700

    Extension Method to display products

---
+++ Lab-6/ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using calculatorLibrary;
+using System;
+
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World!";
+            int n = str.countVowels();
+            Console.WriteLine(n);
+
+
+            Calculator cal = new Calculator
+            {
+                Num1 = 3,
+                Num2 = 8
+
+            };
+            Console.WriteLine(cal.mul());
+            Console.WriteLine(cal.add());
+
+
+        }
+    }
+}
---
+++ Lab-6/ExtensionMethod/ExtensionMethod/extensionMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using calculatorLibrary;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethod
+{
+    public static class extensionmethods
+    {
+        public static int countVowels(this string s)
+        {
+            int vowels = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                {
+                    vowels++;
+                }
+            }
+            return vowels;
+
+        }
+
+        public static int mul(this Calculator c)
+        {
+            int res = c.Num1 * c.Num2;
+            return res;
+        }
+
+    }
+}
