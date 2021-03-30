f1ba988dcb6b7560e3298ec62a0b23cb3297280f
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-03-30 10:16:40 -0700

    Action Delegate

---
+++ Lab-18/Action delegate/Action delegate/Cal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Action_delegate
+{
+    class Cal<T>
+    {
+        public T echo { get; set; }
+        public void Display(Action displayType)
+        {
+            displayType();
+        }
+
+    }
+}
---
+++ Lab-18/Action delegate/Action delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Action_delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cal<int> echInt = new Cal<int>
+            {
+                echo = 9
+            };
+
+            Cal<Product> echProduct = new Cal<Product>
+            {
+                echo = new Product
+                {
+                    ID = 4,
+                    Title = "Audi",
+                    Price = 30000000
+                }
+            };
+
+            Action intDisplay = () => { Console.WriteLine($"Square of 9: {echInt.echo * echInt.echo}."); };
+            Action productDisplay = () => { Console.WriteLine($"Price of {echProduct.echo.Title} is {echProduct.echo.Price}."); };
+
+            echInt.Display(intDisplay);
+            echInt.Display(productDisplay);
+        }
+    }
+}
